2246f2ef494cc7a5f43154f0240d63a10a6328f4
Pablo Fernandez <fernandezpablo85@gmail.com>
2017-03-18 21:19:18 -0300

    add perceptron algorithm and classification

---
+++ python-ml-book/code/perceptron.py
@@ -0,0 +1,23 @@
+from __future__ import division
+import numpy as np
+
+def net_input(xi, yi, weights):
+    return np.dot(xi, weights[1:]) + weights[0]
+
+def predict(xi, yi, weights):
+    return np.where(net_input(xi, yi, weights) >= 0, 1, -1)
+
+def fit(X, y, learning_rate=0.01, iterations=10):
+    number_of_features = X.shape[1]
+    weights = np.zeros(number_of_features + 1)  # +1 for bias weight
+    total_errors = []
+    for i in range(iterations):
+        iteration_errors = 0
+        for xi, yi in zip(X, y):
+            predicted = predict(xi, yi, weights)
+            weights[1:] += learning_rate * (yi - predicted) * xi
+            weights[0] += learning_rate * (yi - predicted)
+            if predicted != yi:
+                iteration_errors += 1
+        total_errors.append(iteration_errors)
+    return weights, total_errors
