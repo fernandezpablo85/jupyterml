0f20059c860f10cd77228f9c5998a6e6fd99d169
Pablo Fernandez <fernandezpablo85@gmail.com>
2017-03-18 22:23:31 -0300

    no need to pass real class to predict/net_input method

--- python-ml-book/code/perceptron.py
+++ python-ml-book/code/perceptron.py
@@ -1,20 +1,20 @@
 from __future__ import division
 import numpy as np
 
-def net_input(xi, yi, weights):
+def net_input(xi, weights):
     return np.dot(xi, weights[1:]) + weights[0]
 
-def predict(xi, yi, weights):
-    return np.where(net_input(xi, yi, weights) >= 0, 1, -1)
+def predict(xi, weights):
+    return np.where(net_input(xi, weights) >= 0, 1, -1)
 
 def fit(X, y, learning_rate=0.01, iterations=10):
     number_of_features = X.shape[1]
     weights = np.zeros(number_of_features + 1)  # +1 for bias weight
     total_errors = []
-    for i in range(iterations):
+    for _ in range(iterations):
         iteration_errors = 0
         for xi, yi in zip(X, y):
-            predicted = predict(xi, yi, weights)
+            predicted = predict(xi, weights)
             weights[1:] += learning_rate * (yi - predicted) * xi
             weights[0] += learning_rate * (yi - predicted)
             if predicted != yi:
